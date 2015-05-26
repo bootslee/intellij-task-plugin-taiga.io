4977ed6ed3bbbd341afcf1af23ed41323711a424
Trevor Jones <trevorjones141@gmail.com>
2015-05-25 20:22:41 -0600

    connection works for testing. need to setup the task and editor options.

--- src/com/busybusy/intellij/taiga/TaigaRepository.java
+++ src/com/busybusy/intellij/taiga/TaigaRepository.java
@@ -5,16 +5,22 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.tasks.CustomTaskState;
 import com.intellij.tasks.Task;
 import com.intellij.tasks.generic.TemplateVariable;
 import com.intellij.tasks.impl.BaseRepositoryImpl;
 import org.apache.commons.httpclient.HttpMethod;
 import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.httpclient.NameValuePair;
 import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.lang.StringUtils;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Set;
 
 /**
  * Created by Tjones on 5/21/15.
@@ -25,9 +31,12 @@ public class TaigaRepository extends BaseRepositoryImpl {
 
     private String mAuthKey = null;
 
-    @NonNls public static final String SERVER_URL = "serverUrl";
-    @NonNls public static final String USERNAME = "username";
-    @NonNls public static final String PASSWORD = "password";
+    @NonNls
+    public static final String SERVER_URL = "serverUrl";
+    @NonNls
+    public static final String USERNAME = "username";
+    @NonNls
+    public static final String PASSWORD = "password";
 
     private final TemplateVariable.FactoryVariable myServerTemplateVariable = new TemplateVariable.FactoryVariable(SERVER_URL) {
         @NotNull
@@ -78,6 +87,17 @@ public class TaigaRepository extends BaseRepositoryImpl {
 
     @NotNull
     @Override
+    public Set<CustomTaskState> getAvailableTaskStates(@NotNull Task task) throws Exception {
+        return super.getAvailableTaskStates(task);
+    }
+
+    @Override
+    public Task[] getIssues(@Nullable String query, int offset, int limit, boolean withClosed, @NotNull ProgressIndicator cancelled) throws Exception {
+        return super.getIssues(query, offset, limit, withClosed, cancelled);
+    }
+
+    @NotNull
+    @Override
     @SuppressWarnings("CloneDoesntCallSuperClone")
     public TaigaRepository clone() {
         return new TaigaRepository(this);
@@ -89,21 +109,7 @@ public class TaigaRepository extends BaseRepositoryImpl {
         return new CancellableConnection() {
             @Override
             protected void doTest() throws Exception {
-                JsonArray response = executeMethod(getAuthMethod());
-                if(response != null && response.get(0) == null)
-                {
-                    JsonObject body = (JsonObject) response.get(0);
-                    if(body.has("_error_message")) {
-                        throw new Exception("Authentication failed with server error: " + body.get("_error_message").getAsJsonPrimitive().getAsString());
-                    }
-                    else {
-                        setAuthKey(body);
-                    }
-                }
-                else
-                {
-                    throw new Exception("Unknown error: Auth method body came back null.");
-                }
+                TaigaRepository.this.doTest();
             }
 
             @Override
@@ -113,37 +119,91 @@ public class TaigaRepository extends BaseRepositoryImpl {
         };
     }
 
+    private void doTest() throws Exception {
+        mAuthKey = null;
+        checkSetup();
+        JsonArray response = executeMethod(getAuthMethod());
+        if (response != null && response.get(0) != null) {
+            JsonObject body = (JsonObject) response.get(0);
+            if (body.has("_error_message")) {
+                throw new Exception("Authentication failed with server error: " + body.get("_error_message").getAsJsonPrimitive().getAsString());
+            } else {
+                setAuthKey(body);
+            }
+        } else {
+            throw new Exception("Unknown error: Auth method body came back null.");
+        }
+    }
+
     @Override
     public boolean isConfigured() {
         boolean result = true;
         if (!super.isConfigured()) {
             result = false;
         }
-        if (result && StringUtil.isEmpty(myServerTemplateVariable.getValue()))
-        {
+        if (result && StringUtil.isEmpty(myServerTemplateVariable.getValue())) {
             result = false;
         }
-        if (result && StringUtil.isEmpty(myUserNameTemplateVariable.getValue()))
-        {
+        if (result && StringUtil.isEmpty(myUserNameTemplateVariable.getValue())) {
             result = false;
         }
-        if (result && StringUtil.isEmpty(myPasswordTemplateVariable.getValue()))
-        {
+        if (result && StringUtil.isEmpty(myPasswordTemplateVariable.getValue())) {
             result = false;
         }
         return result;
     }
 
-    private JsonArray executeMethod(HttpMethod method) throws Exception {
-        method.addRequestHeader("Content-type","application/json");
-        if(mAuthKey != null)
-        {
+    public void checkSetup() throws Exception {
+        String result = "";
+        int errors = 0;
+        if (StringUtil.isEmpty(myServerTemplateVariable.getValue())) {
+            result += "Server";
+            errors++;
+        }
+        if (StringUtil.isEmpty(myUserNameTemplateVariable.getValue())) {
+            result += !StringUtils.isEmpty(result) ? " & " : "";
+            result += "Username";
+            errors++;
+        }
+        if (StringUtil.isEmpty(myPasswordTemplateVariable.getValue())) {
+            result += !StringUtils.isEmpty(result) ? " & " : "";
+            result += "Password";
+            errors++;
+        }
+        if (!result.isEmpty()) {
+            throw new Exception(result + ((errors > 1) ? " are required" : " is required"));
+        }
+    }
+
+    private JsonArray executeMethod(@NotNull HttpMethod method) throws Exception {
+
+        if (mAuthKey != null) {
+            method.addRequestHeader("Content-type", "application/json");
             method.addRequestHeader("Authorization", "Bearer " + mAuthKey);
+        } else {
+            method.addRequestHeader("Content-type", "application/x-www-form-urlencoded");
         }
         getHttpClient().executeMethod(method);
 
+        if (method.getStatusCode() == HttpStatus.SC_UNAUTHORIZED) {
+            JsonObject json = new JsonParser().parse(method.getResponseBodyAsString()).getAsJsonObject();
+            if (json.has("_error_message") && json.get("_error_message").getAsJsonPrimitive().getAsString().equals("Invalid token")) {
+                doTest();
+                getHttpClient().executeMethod(method);
+            }
+
+        }
+
         JsonElement json = new JsonParser().parse(method.getResponseBodyAsString());
-        JsonArray responseBody = json.getAsJsonArray();
+
+        JsonArray responseBody;
+
+        if (json.isJsonArray()) {
+            responseBody = json.getAsJsonArray();
+        } else {
+            responseBody = new JsonArray();
+            responseBody.add(json);
+        }
 
         if (method.getStatusCode() != HttpStatus.SC_OK) {
             throw new Exception("Request failed with HTTP error: " + method.getStatusText());
@@ -151,19 +211,22 @@ public class TaigaRepository extends BaseRepositoryImpl {
         return responseBody;
     }
 
-    private HttpMethod getAuthMethod()
-    {
-        return new PostMethod(getUrl() + kAuthEndpoint);
+    private HttpMethod getAuthMethod() {
+        PostMethod postMethod = new PostMethod(getUrl() + kAuthEndpoint);
+
+        NameValuePair[] data = {
+                new NameValuePair("type", "normal"),
+                new NameValuePair("username", myUsername),
+                new NameValuePair("password", myPassword)
+        };
+        postMethod.setRequestBody(data);
+        return postMethod;
     }
 
-    private void setAuthKey(JsonObject body) throws Exception
-    {
-        if(body.has("auth_token"))
-        {
+    private void setAuthKey(JsonObject body) throws Exception {
+        if (body.has("auth_token")) {
             mAuthKey = body.get("auth_token").getAsJsonPrimitive().getAsString();
-        }
-        else
-        {
+        } else {
             throw new Exception("auth_token missing from server response");
         }
     }
